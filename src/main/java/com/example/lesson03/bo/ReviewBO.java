b0599a83daeb84c5e3a1b3bd50fae10e7fbc77ca
DESKTOP-DGKU219\User <qhdms136@naver.com>
2023-03-22 21:43:31 +0900

    20230322수업

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -17,4 +17,16 @@ public class ReviewBO {
 	public Review getReview(int id) {
 		return reviewMapper.selectReview(id);
 	}
+	
+	// intput : review
+	// output : int (성공한 행의 개수)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "마이켈", 5.0, "역시 맛있따!!");
+	public int addReviewAsField(int storeId111, String menu111, String userName111,
+			Double point111, String review111) {
+		return reviewMapper.insertReviewAsField(storeId111, menu111, userName111, point111, review111);
+	}
 }
--- src/main/java/com/example/lesson03/dao/ReviewMapper.java
+++ src/main/java/com/example/lesson03/dao/ReviewMapper.java
@@ -9,4 +9,16 @@ import com.example.lesson03.model.Review;
 public interface ReviewMapper {
 	
 	public Review selectReview(@Param("id") int id);
+	
+	public int insertReview(Review review);
+	
+	// @Param이 있기 때문에 하나의 맵이 xml로 넘어간다.
+	public int insertReviewAsField(
+			// @Param 명은 중요하다 함부로 마음대로 x
+			// 나머지 변수 명은 마음대로 
+			@Param("storeId") int storeId1234, 
+			@Param("menu") String menu1234, 
+			@Param("userName") String userName1234,
+			@Param("point") Double point1234, 
+			@Param("review") String review1234);
 }
