8d9feae9fd6c81d1a773650515bfadcee2e9e830
DESKTOP-DGKU219\User <qhdms136@naver.com>
2023-03-24 21:48:23 +0900

    20230324수업

--- src/main/java/com/example/lesson04/bo/UserBO.java
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson04.dao.UserMapper;
+import com.example.lesson04.model.User;
 
 @Service
 public class UserBO {
 	@Autowired
 	private UserMapper userMapper;
 	
-	public int addUserAsField(String name, String yyyymmdd, String email, String introduce) {
-		return userMapper.insertUserAsField(name, yyyymmdd, email, introduce);
+	public void addUserAsField(String name, String yyyymmdd, String email, String introduce) {
+		userMapper.insertUserAsField(name, yyyymmdd, email, introduce);
+	}
+	
+	// input : X
+	// output : User(최신 가입자)
+	public User getLatestUser() {
+		return userMapper.selectLatestUser();
 	}
 }
